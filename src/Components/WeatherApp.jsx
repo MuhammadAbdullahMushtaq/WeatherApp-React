ac0fa0736ce00bf43cbb317bf1b1eaa0e893348a
MuhammadAbdullahMushtaq <abdullahmushtaq08@gmail.com>
2023-02-25 17:16:53 +0500

    Finished

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,41 +1,77 @@
 import React, { useEffect, useState } from "react";
+import "./weatherApp.css";
 
 const WeatherApp = () => {
   const [weatherData, setWeatherData] = useState({});
-  const [cityName, setCityName] = useState("");
-  const [searchCityState, setSearchCityState] = useState("");
+  const [cityName, setCityName] = useState("Karachi");
+  const [locationCity, setLocationCity] = useState({});
+  const [searchCityState, setSearchCityState] = useState("Karachi");
+
   useEffect(() => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCityState}&appid=aeea4a77412d6c1856df8675ff429df6&units=metric`)
-    .then((res) => res.json())
-    .then((result) => {
-      setWeatherData(result);
-    })
-    .catch((err) => {
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function (position) {
+            console.log(position);
+            setLocationCity(position);
+            setCityName("");
+          },
+          function (error) {
+            console.log("error", error);
+            setSearchCityState(cityName);
+          }
+        );
+      } else {
+        alert("Geolocation is not supported by this browser.");
+      }
+    }
+    getLocation();
+  }, []);
+
+  useEffect(() => {
+    let searchQuery =
+      locationCity && locationCity.coords
+        ? `lat=${locationCity.coords.latitude}&lon=${locationCity.coords.longitude}`
+        : `q=${searchCityState ? searchCityState : cityName}`;
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?${searchQuery}&appid=aeea4a77412d6c1856df8675ff429df6&units=metric`
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setWeatherData(result);
+      })
+      .catch((err) => {
         console.log("0rrr", err);
       });
-  }, [searchCityState]);
-  
+  }, [searchCityState, locationCity]);
+
   const searchCity = (e) => {
     setSearchCityState(cityName);
+    setLocationCity({});
   };
+
   return (
     <div>
       <div
-        className="d-flex justify-content-center my-5"
-        style={{ flexDirection: "column", alignItems: "center" }}
+        className="d-flex flex-column align-items-center justify-content-center my-5"
       >
+        <h1 style={{color: "white", marginBottom: '50px'}}>WeatherApp</h1>
         <input
           type="text"
           value={cityName}
-          className="form-group form-control w-50"
+          className="form-group form-control media"
           placeholder="Enter City name"
           onChange={(e) => setCityName(e.target.value)}
         />
-        <button onClick={searchCity} className="btn btn-info mb-5">
+        <button onClick={searchCity} className="btn btn-info my-5">
           Search
         </button>
-      <li className="list-unstyled font-weight-bold text-warning h5" >CITY: {weatherData && weatherData.name} </li>
-      <li className="list-unstyled font-weight-bold text-warning h5" >TEMP: {weatherData && weatherData.main && weatherData.main.temp} </li>
+      </div>
+      <div className="listBox">
+      <li className="list-unstyled font-weight-bold text-warning h5" >City: {weatherData && weatherData.name} </li>
+      <li className="list-unstyled font-weight-bold text-warning h5" >Temp: {weatherData && weatherData.main && weatherData.main.temp} </li>
       <li className="list-unstyled font-weight-bold text-warning h5" >
         Condition:{" "}
         {weatherData &&
